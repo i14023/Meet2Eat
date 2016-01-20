0ae20ecf19c1bd7aa1d55c73741837c8eb553b06
Anglea W <i14023@hb.dhbw-stuttgart.de>
2016-01-20 19:36:52 +0100

    Upload
    Testing

---
+++ app/src/main/java/com/example/dreher/meet2eat/Registry.java
@@ -0,0 +1,45 @@
+package com.example.dreher.meet2eat;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Registry extends AppCompatActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etEmail, etUsername, etPassword, etconPassword;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registry);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etconPassword = (EditText) findViewById(R.id.etconPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bRegister:
+
+                break;
+        }
+    }
+}
